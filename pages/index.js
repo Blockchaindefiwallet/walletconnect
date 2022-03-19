bc3844fccfa29054a475a79b8715d139615b46ee
Blockchaindefiwallet <101909666+Blockchaindefiwallet@users.noreply.github.com>
2022-03-19 00:14:53 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,1 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Wallet Connect!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Wallet Connect!" />
-        <p className="description">
-          
